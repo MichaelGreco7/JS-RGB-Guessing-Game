20c1ad8b4ae43b3d7632fa22196b5ad151cd21c0
Michael Greco <michael.greco07@gmail.com>
2020-02-19 14:03:23 -0500

    html and basic css setup

---
+++ app.js
@@ -0,0 +1,16 @@
+// Make a list of colors
+var colors = [
+  "rgb(255, 0, 0)",
+  "rgb(255, 255, 0)",
+  "rgb(0, 255, 0)",
+  "rgb(0, 255, 255)",
+  "rgb(0, 0, 255)",
+  "rgb(0, 255, 255)"
+];
+
+// Select all six squares and loop through them
+// and then assign one of those colors to each background
+
+var squares = document.querySelectorAll(".square");
+// Loop through each one
+for (var i = 0; i < sqaures.length; i++) {}
