c7a0edd5690cdd8132a13c9ceb60d9e1016aefaf
Michael Greco <michael.greco07@gmail.com>
2020-02-19 14:08:38 -0500

    color javascript

--- app.js
+++ app.js
@@ -5,12 +5,15 @@ var colors = [
   "rgb(0, 255, 0)",
   "rgb(0, 255, 255)",
   "rgb(0, 0, 255)",
-  "rgb(0, 255, 255)"
+  "rgb(255, 0, 255)"
 ];
 
 // Select all six squares and loop through them
 // and then assign one of those colors to each background
 
+// Select each one
 var squares = document.querySelectorAll(".square");
 // Loop through each one
-for (var i = 0; i < sqaures.length; i++) {}
+for (var i = 0; i < squares.length; i++) {
+  squares[i].style.backgroundColor = colors[i];
+}
